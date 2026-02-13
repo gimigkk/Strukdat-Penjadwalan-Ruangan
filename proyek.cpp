42d38e23b56502b6f5e9bde9c6276e2ff051b944
julius_Calvin <juliuscalvin_kurniadi@apps.ipb.ac.id>
2026-02-13 20:25:10 +0700

    style : remove whitespace

--- proyek.cpp
+++ proyek.cpp
@@ -57,15 +57,15 @@ class Ruangan {
         string getId() const { return id; }
         string getNamaRuangan() const { return namaRuangan; }
 
-            void printJadwal() {
-                cout << "Ruangan: "<< getNamaRuangan() << endl;
-                cout << "Jadwal" << endl;
-                cout << "=========================" << endl;
-                for (auto& it : daftarJadwal) {
-                    cout << "Kegiatan: " << it.getNamaKegiatan() << endl;
-                    cout << "Jam Mulai: " << formatTime(it.getMulai()) << "\nJam Selesai: " << formatTime(it.getSelesai()) << endl;
-                    cout << "-------------------------" << endl;
-                }
+        void printJadwal() {
+            cout << "Ruangan: "<< getNamaRuangan() << endl;
+            cout << "Jadwal" << endl;
+            cout << "=========================" << endl;
+            for (auto& it : daftarJadwal) {
+                cout << "Kegiatan: " << it.getNamaKegiatan() << endl;
+                cout << "Jam Mulai: " << formatTime(it.getMulai()) << "\nJam Selesai: " << formatTime(it.getSelesai()) << endl;
+                cout << "-------------------------" << endl;
+            }
         }
 };
 
